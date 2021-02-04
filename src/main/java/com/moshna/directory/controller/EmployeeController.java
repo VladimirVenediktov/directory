03de7ac8e77fc8c648e9f895e8e389a6c0018c51
Ivan Moshna <ittifock@gmail.com>
2021-02-04 18:36:49 +0700

    add authentication

--- src/main/java/com/moshna/directory/controller/EmployeeController.java
+++ src/main/java/com/moshna/directory/controller/EmployeeController.java
@@ -10,10 +10,7 @@ import com.moshna.directory.service.MainService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
---
+++ src/main/java/com/moshna/directory/service/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.moshna.directory.service;
+
+import com.moshna.directory.model.MyUserDetails;
+import com.moshna.directory.model.User;
+import com.moshna.directory.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepo userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User user = userRepository.getUserByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Could not find user");
+        }
+
+        return new MyUserDetails(user);
+    }
+
+}
