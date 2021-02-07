7312f042fc88d0deed2c963a107691c213ba0fbd
Ivan Moshna <ittifock@gmail.com>
2021-02-07 18:33:22 +0700

    add DepartmentService

--- src/main/java/com/moshna/directory/controller/DepartmentController.java
+++ src/main/java/com/moshna/directory/controller/DepartmentController.java
@@ -2,27 +2,24 @@ package com.moshna.directory.controller;
 
 import com.moshna.directory.model.Department;
 import com.moshna.directory.repo.DepartmentRepo;
+import com.moshna.directory.service.DepartmentService;
 import com.moshna.directory.service.MainService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @Controller
 public class DepartmentController {
 
-    private DepartmentRepo departmentRepo;
-    private final MainService mainService;
+    private final DepartmentService departmentService;
 
     private static final String DEPARTMENT_ADDING = "department-adding";
 
-    public DepartmentController(DepartmentRepo departmentRepo, MainService mainService) {
-        this.departmentRepo = departmentRepo;
-        this.mainService = mainService;
+    public DepartmentController(DepartmentService departmentService) {
+        this.departmentService = departmentService;
     }
 
     @GetMapping("/department")
@@ -33,19 +30,6 @@ public class DepartmentController {
     @PostMapping("/department")
     public String departmentPostAdd(@Valid Department department,
                                     Model model) {
-        //List<Department> departmentList = mainService.getDepartmentList();
-        String message = "";
-
-        try {
-            departmentRepo.save(department);
-            //model.addAttribute("departmentList", departmentList);
-
-            return "redirect:/home";
-        } catch (Exception e) {
-            message = "validation error";
-            //model.addAttribute("validationMessage", message);
-            return DEPARTMENT_ADDING;
-        }
-
+        return departmentService.departmentPostAdd(department, model);
     }
 }
---
+++ src/main/java/com/moshna/directory/service/DepartmentService.java
@@ -0,0 +1,36 @@
+package com.moshna.directory.service;
+
+import com.moshna.directory.model.Department;
+import com.moshna.directory.repo.DepartmentRepo;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import javax.validation.Valid;
+@Service
+public class DepartmentService {
+
+    private DepartmentRepo departmentRepo;
+
+    private static final String DEPARTMENT_ADDING = "department-adding";
+
+    public DepartmentService(DepartmentRepo departmentRepo) {
+        this.departmentRepo = departmentRepo;
+    }
+
+    public String departmentPostAdd(@Valid Department department,
+                                    Model model) {
+        String message = "";
+
+        try {
+            departmentRepo.save(department);
+            //model.addAttribute("departmentList", departmentList);
+
+            return "redirect:/home";
+        } catch (Exception e) {
+            message = "validation error";
+            model.addAttribute("validationMessage", message);
+            return DEPARTMENT_ADDING;
+        }
+
+    }
+}
